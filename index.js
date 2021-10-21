514c932cbbac40d6eaf003b9bacde19be753f783
ankitchoudhary87 <ankitchoudhary87@gmail.com>
2021-10-21 21:53:47 +0530

    commit updated file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const connectDB = require('./utils/dbConnection')
 const env = require('dotenv');
-//const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 //const cookieParser = require('cookie-parser');
 
 // environment variable or you can say constants
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded())
 app.use(cors())
-//app.use(bodyParser.json())
+app.use(bodyParser.json())
 //app.use(cookieParser());
 app.use(express.static(__dirname + '/public'));
 // Routes
